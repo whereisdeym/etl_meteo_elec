069da81c205e2c0acbc6ca713bb3bcbc34707bac
Draven <deymarbab201.db@gmail.com>
2025-10-19 21:39:48 +0200

    pushing my etl_project

---
+++ scripts/config.py
@@ -0,0 +1,4 @@
+RTE_API_URL = "https://opendata.reseaux-energies.fr/api/explore/v2.1/catalog/datasets/consommation-quotidienne-brute-regionale/records"
+METEO_API_URL = "https://api.openweathermap.org/data/2.5/weather"
+BQ_PROJECT = "etl-meteo-elec"
+BQ_DATASET = "etl_meteo_elec"
